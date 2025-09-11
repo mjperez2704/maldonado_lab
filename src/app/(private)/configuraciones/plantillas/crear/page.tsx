9b416a5a299f9e1dc9e543b87f784127f2615d8c
Misael Josué Pérez Gamboa <misael.perez@mega-solutions.com.mx>
2025-09-11 17:22:08 +0000

    File changes

--- src/app/(private)/configuraciones/plantillas/crear/page.tsx
+++ src/app/(private)/configuraciones/plantillas/crear/page.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { FileText, Save, GripVertical, Settings, Square, Heading1, Heading2, Type, Pilcrow, Image, Barcode, List, AlignLeft } from "lucide-react";
+import { FileText, Save, GripVertical, Settings, Square, Heading1, Heading2, Type, Pilcrow, Image, Barcode, List, AlignLeft, User, Calendar, VenetianMask, Microscope } from "lucide-react";
 import React, { useState } from 'react';
 import Link from "next/link";
 import { Separator } from "@/components/ui/separator";
